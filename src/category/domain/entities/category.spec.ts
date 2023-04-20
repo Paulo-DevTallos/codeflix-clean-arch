76ea1611d2a1c6762f449198dd61d75be05b1ab4
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-20 15:16:18 -0300

    test: criando testes para o construtor

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,23 @@
 import { Category } from "./category"
 
 describe('Category unit tests', () => {
-    it('Should test the constructor of category', () => {
-        const category = new Category('Movie')
-
-        expect(typeof category.name).toBe('string')
+  // forma de teste utilizando a técnica do triple A - Arrange / Act / Assert
+  it('Should test the constructor of category', () => {
+    //arrange
+    const create_at = new Date()
+    // act
+    const category = new Category({
+      name: 'Movie',
+      description: 'any description',
+      is_active: true,
+      create_at,
     })
+    // assert
+    expect(category.props).toStrictEqual({
+      name: 'Movie',
+      description: 'any description',
+      is_active: true,
+      create_at,
+    })
+  })
 })
