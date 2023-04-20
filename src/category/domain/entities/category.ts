be45fa5f51828e3e58bea9d6f2839a3ae5159a49
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-20 17:31:23 -0300

    feat: criando logica para a criacao de categoria

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -3,29 +3,39 @@ export type CategoryProps = {
   name: string,
   description?: string,
   is_active?: boolean,
-  create_at?: Date
+  created_at?: Date
 }
 
 export class Category {
-  constructor(public readonly props: CategoryProps) {}
+  constructor(public readonly props: CategoryProps) {
+    this.props.description = this.props.description
+    this.props.is_active = this.props.is_active ?? true
+    this.props.created_at = this.props.created_at ?? new Date()
+  }
 
   get name():string {
     return this.props.name
   }
 
-  get description(): string | undefined {
+  get description() {
     return this.props.description
+  }
+
+  private set description(value: string) {
+    this.props.description = value ?? null
   }
 
   get is_active() {
     return this.props.is_active
   }
 
+  private set is_active(value: boolean) {
+    this.props.is_active = this.props.is_active ?? true
+  }
+
   get created_at() {
-    return this.props.create_at
+    return this.props.created_at
   }
 }
-
-const category = new Category({ name: 'Movie' })
 
 // create
