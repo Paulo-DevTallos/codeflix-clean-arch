3cd7fd6d311bf774b7959d22d5224b4aca10fb85
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-20 20:37:58 -0300

    test: implementando teste de criação de categoria por nome

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,23 +1,37 @@
+import { omit } from "lodash"
 import { Category } from "./category"
 
 describe('Category unit tests', () => {
   // forma de teste utilizando a técnica do triple A - Arrange / Act / Assert
   it('Should test the constructor of category', () => {
     //arrange
-    const create_at = new Date()
+    const created_at = new Date()
     // act
     const category = new Category({
       name: 'Movie',
       description: 'any description',
       is_active: true,
-      create_at,
+      created_at,
     })
     // assert
     expect(category.props).toStrictEqual({
       name: 'Movie',
       description: 'any description',
       is_active: true,
-      create_at,
+      created_at,
+    })
+  })
+
+  it('Should test the creation of category by name', () => {
+    const category = new Category({ name: 'Movies' })
+
+    let props = omit(category.props, 'created_at')
+    expect(props).toStrictEqual({
+      name: 'Movies',
+      description: null,
+      is_active: true,
     })
   })
 })
+
+//expect(category.props.created_at).toBeInstanceOf(Date)
