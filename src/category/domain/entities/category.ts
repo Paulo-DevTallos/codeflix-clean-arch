5f309be61d1f8af9b352c24193fcba5427ef2468
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-05-03 21:38:12 -0300

    feat: implementando id da categoria

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 // criando uma entity
 export type CategoryProps = {
   name: string,
@@ -7,34 +9,38 @@ export type CategoryProps = {
 }
 
 export class Category {
-  constructor(public readonly props: CategoryProps) {
-    this.description = this.props.description
-    this.props.is_active = this.props.is_active ?? true
-    this.props.created_at = this.props.created_at ?? new Date()
+
+  public readonly id: string;
+
+  constructor(public readonly props: CategoryProps, id?: string) {
+    this.id = id || uuidv4();
+    this.description = this.props.description;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
   }
 
   get name():string {
-    return this.props.name
+    return this.props.name;
   }
 
   get description() {
-    return this.props.description
+    return this.props.description;
   }
 
   private set description(value: string) {
-    this.props.description = value ?? null
+    this.props.description = value ?? null;
   }
 
   get is_active() {
-    return this.props.is_active
+    return this.props.is_active;
   }
 
   private set is_active(value: boolean) {
-    this.props.is_active = value ?? true
+    this.props.is_active = value ?? true;
   }
 
   get created_at() {
-    return this.props.created_at
+    return this.props.created_at;
   }
 }
 
